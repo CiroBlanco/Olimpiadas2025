a4a7bc1e0f4f4a888b5d3ad18931208e0090298e
Moraa-Gutierrez <gutierrezmora0609@gmail.com>
2025-06-11 16:53:52 -0300

    11/06

--- componentes/menu_index.php
+++ componentes/menu_index.php
@@ -1,3 +1,18 @@
 <?php
-
+include_once('conexion.php');
+session_start();
+if(!isset($_SESSION['usuario'])){ //si no hay sesion
+   echo'<header class="header">
+        <nav>
+            <ul>
+                <li><a href="index.php" class="active">Paquetes Turisticos</a></li>
+                <li><a href="sedes.php">Alquiler Vehiculos</a></li>
+                <li><a href="quienessomos.php">Estadias</a></li>
+                <li><a href="donar.php">Paasajes Aereos</a></li>
+                <li><a href="formulario_registrarse.php">Registrarse</a></li>
+                <li><a href="formulario_iniciarsesion.php">Inicar Sesion</a></li>
+            </ul>
+       </nav>
+     </header>';
+}
 ?>
--- index.php
+++ index.php
@@ -3,17 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./componentesinicio/index.css">
+    <link rel="stylesheet" href="index.css">
     <title>olimpiadas2025</title>
 </head>
 <body>
     <?php
         //incluimos el menu
-        include('componentes/menu_index.php');
+        include('menu_index.php');
      ?>
-     <h1>Agencia de Viajes</h1>
+     <h1 class="titulos">Agencia de Viajes</h1>
+     <h2 class="titulos">Tecno Travel</h2> 
+     <img src="imageneslogo/logo(2).png" alt="">
      <div class="contenedor">
-     <img src="" alt="">       
+       
      </div>
      <?php
      include('componentesinicio/footer.php')
