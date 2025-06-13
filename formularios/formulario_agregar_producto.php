d9ea931a84adf7ad6b33fac69685cb5d95996986
CiroBlanco <blancociro43@Gmail.com>
2025-06-12 22:58:56 -0300

    12/6

--- formularios/formulario_agregar_producto.php
+++ formularios/formulario_agregar_producto.php
@@ -7,8 +7,27 @@
     <link rel="stylesheet" href="../index.css">
 </head>
 <body>
-    <form action="agregar_producto.php" method= $_POST>
-        
+    <h1 class="titulos">Agregar Producto</h1>
+    <form action="agregar_producto.php" method="POST">
+
+     <div class="campo">
+        <label for="nombre">Nombre Producto:</label>
+        <input type="text" name="nombre" id="nombre" placeholder="" required autocomplete="off">
+     </div>
+
+     <div class="campo">
+        <label for="descripcion">Descripcion:</label>
+        <input type="text" name="descripcion" id="descripcion" placeholder="" required autocomplete="off">
+     </div>
+
+     <div class="campo">
+        <label for="precio_unitario">Precio Unitario:</label>
+        <input type="text" name="precio_unitario" id="precio_unitario" placeholder="" required autocomplete="off">
+     </div>
+
+     <input type="reset" value="Cancelar">
+     <input type="submit" value="Agregar">
+    
     </form>
 </body>
 </html>
