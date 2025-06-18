a59e7583dde4be2ba8b255b949e945687f7ab39c
CiroBlanco <blancociro43@Gmail.com>
2025-06-18 00:02:49 -0300

    18/6

--- componentes/agregar_producto_carrito.php
+++ componentes/agregar_producto_carrito.php
@@ -1,21 +1,16 @@
 <?php
+session_start();
 include('conexion.php');
 
-var_dump($_POST);
-
+$id_usuario = $_SESSION['id_usuario'];
 $id_producto = $_POST['id_producto'];
-$cantidad = $_POST ['cantidad'];
-//$categoria = $_POST['categoria'];
-//$descripcion= $_POST['descripcion'];
-//$precio_unitario=$_POST['preciounitario'];
-//$id_tipo=$_POST['idtipo'];
-$agregar_producto= "INSERT INTO carrito(id_producto,cantidad) VALUES (
-'".$id_producto."',
-'".$cantidad."'
- )";
-    $resultado_agregar = mysqli_query($conexion,$agregar_producto);
+
+$agregar_producto = "INSERT INTO carrito (id_usuario,id_producto) VALUES ('".$id_usuario."','".$id_producto."')";
+
+$resultado_agregar = mysqli_query($conexion,$agregar_producto);
     if (!$resultado_agregar) {
     echo "Error al agregar producto: " . mysqli_error($conexion);
     }
+header("Location: carrito.php"); 
 exit;
 ?>
